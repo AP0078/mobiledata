8ce3880eecd80a5ce4333f8d17e153e873b7c1ec
Aung Phyoe <>
2018-12-24 21:53:10 +0800

    added Response Codable Model

---
+++ Mobile Data/Mobile Data/Model/Fields.swift
@@ -0,0 +1,18 @@
+import Foundation
+struct Fields : Codable {
+	let type : String?
+	let id : String?
+
+	enum CodingKeys: String, CodingKey {
+
+		case type = "type"
+		case id = "id"
+	}
+
+	init(from decoder: Decoder) throws {
+		let values = try decoder.container(keyedBy: CodingKeys.self)
+		type = try values.decodeIfPresent(String.self, forKey: .type)
+		id = try values.decodeIfPresent(String.self, forKey: .id)
+	}
+
+}
---
+++ Mobile Data/Mobile Data/Model/Records.swift
@@ -0,0 +1,21 @@
+import Foundation
+struct Records : Codable {
+	let volume_of_mobile_data : String?
+	let quarter : String?
+	let id : Int?
+
+	enum CodingKeys: String, CodingKey {
+
+		case volume_of_mobile_data = "volume_of_mobile_data"
+		case quarter = "quarter"
+		case id = "_id"
+	}
+
+	init(from decoder: Decoder) throws {
+		let values = try decoder.container(keyedBy: CodingKeys.self)
+		volume_of_mobile_data = try values.decodeIfPresent(String.self, forKey: .volume_of_mobile_data)
+		quarter = try values.decodeIfPresent(String.self, forKey: .quarter)
+		id = try values.decodeIfPresent(Int.self, forKey: .id)
+	}
+
+}
